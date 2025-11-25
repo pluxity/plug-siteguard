2a512ad7402c86f51482610b3c928a6667f8e0e4
Na <nadk@pluxity.com>
2025-11-25 19:45:43 +0900

    hotfix(infra) : 환경변수 설정 및 자동 배포 설정

--- apps/yongin-smart-city/vite.config.ts
+++ apps/yongin-smart-city/vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    port: 3000,
-    open: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: process.env.VITE_BASE_URL || env.VITE_BASE_URL || '/',
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      port: 3000,
+      open: true,
+    },
+  };
 });
